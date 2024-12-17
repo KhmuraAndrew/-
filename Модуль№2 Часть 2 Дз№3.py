a12a53f5f6e0ffc74edb888d5c07d07b993e01ca
Хмура Андрей <khmurahottest@gmail.com>
2024-12-17 05:36:45 +0300

    Add files via upload

---
+++ Модуль№2 Часть 2 Дз№3.py
@@ -0,0 +1,7 @@
+a=int(input("Введите число:"))
+if a<0:
+    print("Number is negative")
+elif a>0:
+    print("Numberis positive")
+else:
+    print("Number is equal to zero")
---
+++ Модуль№2 Часть 2 Дз№4.py
@@ -0,0 +1,8 @@
+a=int(input("Введите число:"))
+b=int(input("Введите число:"))
+if a<b:
+    print(a,b)
+elif b<a:
+    print(b,a)
+else:
+    print(a,b)
